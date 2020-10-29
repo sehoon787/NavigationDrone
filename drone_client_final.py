8ae6858be51778a1117839bf856ce01f6c0397da
Juliansh787 <59464137+Juliansh787@users.noreply.github.com>
2020-10-29 18:01:08 +0900

    Update drone_client_final.py

--- drone_client_final.py
+++ drone_client_final.py
@@ -437,7 +437,7 @@ if __name__=="__main__":
 
     # socket connection address and port for Koren VM TSP server
     # get shortest path data from Koren VM TSP server
-    TSP_SERVER_IP = "192.168.1.221"  # Koren VM TSP server IP
+    TSP_SERVER_IP = "116.89.189.55"  # Koren VM TSP server IP
     TSP_SERVER_PORT = 22042
     SIZE = 512
     tsp_client_socket = socket(AF_INET, SOCK_STREAM)
